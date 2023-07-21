3880e777e663bab1c9d1bc4acec2846d51f03f19
Petro Strynada <petro.strynada@gmail.com>
2023-07-21 11:37:21 +0300

    UICollectionView data sources
    + func collectionView(_ collectionView: UICollectionView,
    numberOfItemsInSection section: Int)
    FOR a certain number of cells
    func collectionView(_ collectionView: UICollectionView, cellForItemAt
    indexPath: IndexPath)
     We already designed a prototype in Interface Builder, and configured the
    PersonCell class for it, so we need to create and return one of these.
    dequeueReusableCell(withReuseIdentifier:for:)
    his creates a collection view cell using the reuse identified we
    specified, in this case "Person" because that was what we typed into
    Interface Builder earlier. But just like table views, this method will
    automatically try to reuse collection view cells, so as soon as a cell
    scrolls out of view it can be recycled so that we don't have to keep
    creating new ones. # Пожалуйста, введите сообщение коммита для ваших
    изменений.
    Строки, # начинающиеся с «#» будут проигнорированы, а пустое сообщение

--- Project10_Names_to_Faces/Project10_Names_to_Faces/ViewController.swift
+++ Project10_Names_to_Faces/Project10_Names_to_Faces/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UICollectionViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //when we call dequeueReusableCell(withReuseIdentifier:for:) we’ll be sent back a regular UICollectionViewCell rather than our custom PersonCell type.
+        //so we use typecast with guard
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        return cell
+    }
+
 
 }
 
